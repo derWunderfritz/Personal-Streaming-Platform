f82b39e46d55b2dc45d4909ac464020da94b8fc0
Fede <fjimenez.x1@gmail.com>
2022-03-23 20:13:53 -0600

    initial commit
    Base project

---
+++ js/movies.js
@@ -0,0 +1,24 @@
+var myArray = [{
+        'id': '0',
+        'name': '',
+        'year': '',
+        'director': '',
+        'source': '',
+        'imdb': ''
+    }, {
+        'id': '1',
+        'name': 'hello',
+        'year': '22',
+        'director': 'gg',
+        'source': '',
+        'imdb': ''
+    }
+    /* {
+        'id': '',
+        'name': '',
+        'year': '',
+        'director': '',
+        'source': '',
+        'imdb': ''
+    },*/
+]
